d80404e4f334c8add0786b11b3bd1b8ce154b20d
Dara Hayes <dara.hayes@nearform.com>
2017-07-13 09:40:13 +0100

    rename database name env variable

--- server/config/index.js
+++ server/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
   pg: {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
-    database: process.env.RH_USERS_DB || 'rh_users',
+    database: process.env.DB_NAME || 'users',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres'
   }
